5e499826b189348be99f7769f8019547de6d8e6f
IrmIris33 <irmirisortizortiz@outlook.com>
2021-08-30 15:37:32 -0400

    First Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,37 @@
+import React, { useState} from 'react';
+import { Form, Navbar } from 'react-bootstrap';
+
+
+const Header = (FetchUrl) => {
+    const [ searchValue, setSearchValue ] = useState('')
+
+    const handleSearchInputChanges = (e) => {
+        e.preventDefault()
+        setSearchValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        FetchUrl(searchValue)
+    }
+    
+    return (
+        <div className = 'container-header'>
+            <Navbar className = 'Navbar' expand = 'lg'>
+                <Navbar.Brand className='header'> What To Watch</Navbar.Brand>
+                <Form onSubmit={handleSubmit} inline className='search-form'>
+                    <Form.Control className='search'
+                        value={searchValue}
+                        onChange={handleSearchInputChanges}
+                        type='text'
+                        placeholder='Search Movie Title'/>
+
+                    <input className='search-button' type='submit' value='SEARCH'/>
+                </Form>
+            </Navbar>
+
+        </div>
+    );
+};
+
+export default Header;
